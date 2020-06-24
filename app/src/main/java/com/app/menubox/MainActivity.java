ef2c0fca5a205d11e8616e26a3371a6e0a545b16
luzhenyuxfcy@sina.com <wdx2lzy25>
2020-06-24 09:51:31 +0800

    添加菜单项按钮动画开关

--- app/src/main/java/com/app/menubox/MainActivity.java
+++ app/src/main/java/com/app/menubox/MainActivity.java
@@ -3,10 +3,10 @@ package com.app.menubox;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.zndroid.menubox.MenuBox;
 import com.zndroid.menubox.core.IMenuItemClick;
@@ -53,7 +53,6 @@ public class MainActivity extends AppCompatActivity {
         list.add(item3);
 
         menuBox = new MenuBox.Builder(this)
-                .setFullScreen(true)
                 .setMenuItemList(list)
                 .setRow(3)
                 .withButtonAnimation(true)
